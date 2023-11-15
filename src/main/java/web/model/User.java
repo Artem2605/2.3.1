81cbad01748f6231d4c00e9f401bd2c9b35575db
Artem2605 <artyom260599@mail.ru>
2023-11-15 03:02:15 +0300

    firstSend

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -11,12 +11,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(name = "first_name")
-    //@NotEmpty(message = "NOT EMPTY")
     @Pattern(regexp = "[a-zA-ZА-Яа-яЁё]+", message = "Неверно указано имя")
     private String firstName;
-
 
     @Column(name = "last_name")
     @Pattern(regexp = "[a-zA-ZА-Яа-яЁё]+", message = "Неверно указана фамилия")
